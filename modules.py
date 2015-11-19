d86ced3a9bd83d2f98721f3c41d7e646ab38ef59
jonsag <jonsagebrand@gmail.com>
2015-11-19 18:10:23 +0100

    Many changes and one new example.config

--- modules.py
+++ modules.py
@@ -3,15 +3,23 @@
 # Encoding: UTF-8
 
 import ConfigParser, os, sys
+from __builtin__ import False
 
 config = ConfigParser.ConfigParser()  # define config file
 config.read("%s/config.ini" % os.path.dirname(os.path.realpath(__file__)))  # read config file
 
+videoExtensions = (config.get('fileTypes', 'videoExtensions').replace(" ", "")).split(',')  # load video extensions
+subExtensions = (config.get('fileTypes', 'subExtensions').replace(" ", "")).split(',')  # load subtitle extensions
+
+
 def onError(errorCode, extra):
-    print "\nError:"
-    if errorCode == 1:
+    print "\nError: %s" % errorCode
+    if errorCode in (1, 4):
         print extra
         usage(errorCode)
+    elif errorCode in (3, 5):
+        print "%s" % extra
+        sys.exit(errorCode)
     elif errorCode == 2:
         print "No options given"
         usage(errorCode)
@@ -22,3 +30,87 @@ def usage(exitCode):
     print "%s " % sys.argv[0]
 
     sys.exit(exitCode)
+    
+def isFile(myFile, extension):
+    result = False
+    fileExtension = os.path.splitext(myFile)[1]
+    if fileExtension.lower() == ".%s" % extension:
+        result = True
+    return result
+    
+def findFiles(searchPath, recursive, extensions, verbose):
+    myFiles = []
+    if verbose:
+        if extensions == "Videos":
+            print "Searching for video files..."
+        elif extensions == "Files":
+            print "Searching for files only..."
+        elif extensions == "*":
+            print "Searching for anything..."
+    
+    if recursive:  # scan directories recursively
+        if verbose:
+            print "\n--- Searching %s recursively for files matching extension list..." % searchPath
+        for root, dirs, files in os.walk(searchPath):
+            for myFile in files:
+                addFile = False
+                if verbose:
+                    print "Checking if %s matches..." % myFile
+                if extensions == "*Videos":
+                    for extension in videoExtensions:
+                        if isFile(os.path.join(str(root), myFile), extension):  # check if myFile matches any of the extensions
+                            addFile = True
+                            break
+                elif extensions == "*Subs":
+                    for extension in subExtensions:
+                        if isFile(os.path.join(str(root), myFile), extension):  # check if myFile matches any of the extensions
+                            addFile = True
+                            break
+                elif extensions == "*Files" and os.path.isfile(myFile) and not os.path.islink(myFile):
+                    addFile = True
+                elif extensions == "*All":
+                    addFile = True
+                else:
+                    if isFile(os.path.join(str(root), myFile), extensions):
+                        addFile = True
+                    
+                if addFile:
+                    if verbose:
+                        print "%s matches" % myFile
+                    myFiles.append(os.path.join(str(root), myFile))
+                    
+    else: # scan only given directory
+        if verbose:
+            print "\n--- Searching %s for files matching extension list..." % searchPath
+        for myFile in os.listdir(searchPath):
+            addFile = False
+            if verbose:
+                print "Checking if %s matches..." % myFile
+            if extensions == "*Videos":
+                for extension in videoExtensions:
+                    if isFile(os.path.join(str(searchPath), myFile), extension):  # check if myFile matches any of the extensions
+                        addFile = True
+                        break
+            elif extensions == "*Subs":
+                for extension in subExtensions:
+                    if isFile(os.path.join(str(searchPath), myFile), extension):  # check if myFile matches any of the extensions
+                        addFile = True
+                        break
+            elif extensions == "*Files" and os.path.isfile(myFile) and not os.path.islink(myFile):
+                addFile = True 
+            elif extensions == "*All":
+                addFile = True
+            else:
+                if isFile(os.path.join(str(searchPath), myFile), extensions):
+                    addFile = True
+                
+            if addFile:
+                if verbose:
+                    print "%s matches" % myFile
+                myFiles.append(os.path.join(str(searchPath), myFile))
+                       
+    if verbose:
+        print "--- Number of matching files in %s: %d" % (searchPath, len(myFiles))
+    
+    return sorted(myFiles)
+
--- pyRename.py
+++ pyRename.py
@@ -2,26 +2,109 @@
 # -*- coding: utf-8 -*-
 # Encoding: UTF-8
 
-import sys, getopt, os
+import sys, getopt, os, ConfigParser
 
-from modules import onError, usage
+from modules import onError, usage, findFiles
+from genericpath import isfile
 
 try:
     myopts, args = getopt.getopt(sys.argv[1:],
-                                 'vh',
-                                 ['verbose', 'help'])
+                                 'c:p:ro:n:e:vh',
+                                 ['configfile=', 'path=', 'recursive', 'oldpattern=', 'newpattern=', 
+                                  'extensions=', 
+                                  'verbose', 'help'])
 
 except getopt.GetoptError as e:
     onError(1, str(e))
 
-if len(sys.argv) == 1:  # no options passed
-    onError(2, 2)
+#if len(sys.argv) == 1:  # no options passed
+#    onError(2, 2)
     
+configFileName = ""
+searchPath = os.path.abspath(os.getcwd())
+recursive = False
+oldPattern = ""
+newPattern = ""
+extensions = "*"
 verbose = False
     
 for option, argument in myopts:
-    if option in ('-v', '--verbose'):  # verbose output
+    if option in ('-c', '--configfile'):
+        configFileName = argument
+    elif option in ('-p', '--path'):
+        searchPath = os.path.abspath(argument)
+    elif option in ('-r', '--recursive'):
+        recursive = True
+    elif option in ('-o', '--oldpattern'):
+        oldPattern = argument
+    elif option in ('-n', '--newpattern'):
+        newPattern = argument 
+    elif option in ('-e', '--extensions'):
+        extensions = argument
+    elif option in ('-v', '--verbose'):  # verbose output
         verbose = True
     elif option in ('-h', '--help'):  # display help text
         usage(0)
         
+if configFileName: # argument -c --configfile passed
+    if not isfile(configFileName): # config file does not exist
+        onError(3, "%s is not a valid file" % configFileName)
+    if verbose:
+        print "\nReading config file...\n"
+    myConfig = {}
+    with open(configFileName, 'r') as configFile:
+        fileContent = configFile.readlines() 
+    for line in fileContent:
+        if verbose:
+            print "Line: %s" % line.rstrip("\n")
+        line = line.lstrip(" ")
+        #line = line.lower()
+        if line.lower().startswith("oldpattern"):
+            oldPattern = line.split("=")[1].lstrip(" ").rstrip("\n")
+            myConfig['Old pattern'] = oldPattern
+        elif line.lower().startswith("newpattern"):
+            newPattern = line.split("=")[1].lstrip(" ").rstrip("\n")
+            myConfig['New pattern'] = newPattern
+        elif line.lower().startswith("searchpath"):
+            searchPath = line.split("=")[1].lstrip(" ").rstrip("\n")
+            if searchPath.startswith("~"):
+                home = os.path.expanduser("~")
+                searchPath = ("%s%s") % (home, searchPath.lstrip("~"))
+            myConfig['Search path'] = searchPath
+        elif line.lower().startswith("recursive"):
+            if (line.lower().split("=")[1].lstrip(" ").rstrip("\n") == "yes" or 
+                line.split("=")[1].lstrip(" ").rstrip("\n") == "1" or 
+                line.lower().split("=")[1].lstrip(" ").rstrip("\n") == "true"):
+                recursive = True 
+            myConfig['Recursive'] = recursive
+            
+    if verbose:
+        print "\nConfig:"
+        for key, value in myConfig.items():
+            print "%s: %s" % (key, value)
+    
+if searchPath:  # argument -p --path passed
+    if not os.path.isdir(searchPath):  # not a valid path
+        onError(4, "%s is not a valid path" % searchPath)
+else:
+    print "\nNo path given."
+    print "Using current dir %s" % searchPath
+        
+myFiles = findFiles(searchPath, recursive, extensions, verbose)
+
+toRename = []
+
+for myFile in myFiles:
+    if oldPattern in myFile:
+        toRename.append(myFile)
+        
+if toRename:
+    print "\nThese files will be renamed:"
+    for myFile in toRename:
+        print "%s => %s" % (os.path.basename(myFile), os.path.basename(myFile.replace(oldPattern, newPattern)))
+                            
+                            
+                            
+                            
+                            
+                            
